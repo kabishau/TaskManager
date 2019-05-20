fd70233e413f22c6e488d542db1305c3df154e17
Aleksey DreamLine <aleksey.kabishau@dreamline.com>
2019-05-20 18:37:32 -0400

    cleaning

--- TaskManager/ViewController.swift
+++ TaskManager/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  TaskManager
-//
-//  Created by user153502 on 5/20/19.
-//  Copyright © 2019 user153502. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
